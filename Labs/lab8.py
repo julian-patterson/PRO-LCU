b364e3e08b289a2fe7a33fa5a43794aadfd964c7
julian-patterson <julianedwardpatterson@gmail.com>
2023-03-02 23:23:00 -0500

    finished function: need to make sure it is right

--- Labs/lab8.py
+++ Labs/lab8.py
@@ -1,5 +1,17 @@
 def check_card_number(x):
-    if len(x) == 16:
-        character_count = True
-    else:
-        character_count = False
+    if len(x) != 16:
+        return False
+
+    for i in x:
+        if i > 9:
+            return False
+        else:
+            continue
+
+    total = 0
+    x.split()
+    for i in x:
+        x[i] = int(x[i])
+        total += x[i]
+    if total % 2 != 0:
+        return False
