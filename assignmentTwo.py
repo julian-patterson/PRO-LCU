56f70dde67dbf45c0ff440c328f4919f28f6489b
julian-patterson <julianedwardpatterson@gmail.com>
2023-03-16 21:11:20 -0400

    variables

--- assignmentTwo.py
+++ assignmentTwo.py
@@ -4,3 +4,12 @@ Julian Patterson 2131249
 S. Hilal, instructor
 Assignment 2
 '''
+
+Names = []  # names
+IDs = []  # students' IDs
+Prog = []  # program codes
+Tests = []  # Test grades (2 grades/student)
+Labs = []  # Lab grades (3 grades/student)
+Proj = []  # Project grades
+Grds = []  # Total grades
+LGrds = []  # letter grades
