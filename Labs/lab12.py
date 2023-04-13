2c04a1e4f64838c33ac4976244d5afb942e3f661
julian-patterson <julianedwardpatterson@gmail.com>
2023-04-13 17:06:53 -0400

    lab12 final commiit

---
+++ Labs/lab12.py
@@ -0,0 +1,22 @@
+'''
+Julian Patterson 2131249
+420-LCU Computer Programming
+S. Hilal, instructor
+Lab 12
+'''
+
+Books = {}
+data = open("books.txt" , "r")
+
+for entry in data.readlines():
+    entry.split("\n")
+    book_record = entry.split(",")
+    keys = ["auth", "lang", "type", "sold"]
+    book_record[-1] = int(book_record[-1])
+    book_dict = dict(zip(keys, book_record[1:]))
+    Books[book_record[0]] = book_dict
+
+x = "{:50s} {:20s}"
+for entry in sorted(Books):
+    # print(entry, Books[entry])
+    print(x.format(str(entry), str(Books[entry])))
