41a86843bb39de14f10342c8b96e10aefd066c7b
julian-patterson <julianedwardpatterson@gmail.com>
2023-04-27 17:11:39 -0400

    lab 13

--- Labs/lab13.py
+++ Labs/lab13.py
@@ -49,36 +49,35 @@ class BankAccount:
     def create_pwd(self):
         pwd_success = False
         pwd = input("Please input a password: ")
-        while pwd_success == False:
-            if 8 <= len(pwd) <= 15:
-                if pwd.isalnum():
-                    if pwd[0].isupper():
-                        if pwd[-4].isdigit():
-                            if not pwd.isupper():
-                                pwd_success = True
-                                break
-                            else:
-                                print(
-                                    "Password incorrect; password must have at least one lower case character")
-                                return False
-                                break
+        if 8 <= len(pwd) <= 15:
+            if pwd.isalnum():
+                if pwd[0].isupper():
+                    if pwd[-4].isdigit():
+                        if not pwd.isupper():
+                            pwd_success = True
+                            
                         else:
                             print(
-                                "Password incorrect; last four characters must be a digit")
+                                "Password incorrect; password must have at least one lower case character")
                             return False
-                            break
+                            
                     else:
-                        print("Password incorrect; first letter must be capitalized")
+                        print(
+                            "Password incorrect; last four characters must be a digit")
                         return False
-                        break
+                        
                 else:
-                    print("Password incorrect; must be only alphanumeric")
+                    print("Password incorrect; first letter must be capitalized")
                     return False
-                    break
+                    
             else:
-                print("Password incorrect; must be between [8: 15] characters")
+                print("Password incorrect; must be only alphanumeric")
                 return False
-                break
+                
+        else:
+            print("Password incorrect; must be between [8: 15] characters")
+            return False
+                
 
         if pwd_success == True:
             print("Password Accepted!")
@@ -108,6 +107,8 @@ for entry in data.readlines():
     account_dict = dict(zip(keys, account[1:]))
     Accounts[account[0]] = account_dict
 
+print(Accounts)
+
 
 # Q2
 x = BankAccount(CODE=122156, NAME="Lea Smith",
@@ -117,4 +118,38 @@ x = BankAccount(CODE=122156, NAME="Lea Smith",
 BankAccount(CODE=222552, NAME="John Green", BANK="RBC", TYPE="RBC")
 
 # Q4
-x.create_pwd("Abcede1234")
+x.create_pwd()
+
+# Q5
+for key in Accounts:
+    if key == 122156:
+        BankAccount.deposit(122156, 100)
+    else:
+        print("This account does not exist")
+
+# Q6
+for key in Accounts:
+    if key  == 888888:
+        BankAccount.deposit(888888, 100)
+    else:
+        print("This account does not exist")
+
+# Q7
+for key in Accounts:
+    if key == 122156:
+        BankAccount.withdraw(122156, 100)
+    else:
+        print("This account does not exist")
+
+# Q8
+for key in Accounts:
+    if key == 122156:
+        BankAccount.get_balance(122156)
+    else:
+        print("This account does not exist")
+
+# Q9
+fmt = "{:6s} {:20s} {:10s} {:10s} {:30s}"
+print(fmt.format("CODE" + "NAME" + "BANK" + "TYPE"+ "BALANCE"))
+for key,value in Accounts.items():
+    print("{:6d}".format(key) + "{:20s}".format(str(value["name"])) + "{:10s}".format(str(value["bank"])) + "{:10s}".format(str(value["type"])) + "{:30s}".format(str(value["balance"])))
